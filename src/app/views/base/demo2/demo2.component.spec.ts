78359723f3ca757440e4c2d47eaad2a74b2b652d
Nguyen Phuong Nam <phuongnamcp@gmail.com>
2021-03-06 14:23:30 +0700

    commit SHC-100001

---
+++ src/app/views/base/demo2/demo2.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { Demo2Component } from './demo2.component';
+
+describe('Demo2Component', () => {
+  let component: Demo2Component;
+  let fixture: ComponentFixture<Demo2Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Demo2Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Demo2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
